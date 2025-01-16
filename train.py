12bf56d8706de23c1a7aefda25b7c450c0f7f902
Deniz <56404667+Schiweppes@users.noreply.github.com>
2025-01-16 15:09:46 +0300

    feat: enhance training process with OneCycleLR scheduler and improved loss calculations

--- train.py
+++ train.py
@@ -1,7 +1,7 @@
 import os
 import torch
 from torch import nn
-from torch.optim.lr_scheduler import CosineAnnealingLR
+from torch.optim.lr_scheduler import CosineAnnealingLR, OneCycleLR
 from tqdm import tqdm
 import matplotlib.pyplot as plt
 from evaluation import evaluate_classification
@@ -20,15 +20,28 @@ class Trainer:
     ):
         self.model = model
         self.optimizer = torch.optim.AdamW(model.parameters(), lr=lr)
+        self.box_loss = nn.SmoothL1Loss(
+            beta=1.0 / 9.0
+        )  # beta=1/9 as per Faster R-CNN paper
+        self.detection_cls_loss = nn.CrossEntropyLoss()
+
         self.train_loader = train_loader
         self.val_loader = val_loader
+
         device = "cuda" if torch.cuda.is_available() else "cpu"
         self.device = torch.device(device)
         self.epochs = epochs
         self.save_dir = save_dir
         os.makedirs(self.save_dir, exist_ok=True)
         self.model.to(self.device)
-        self.scheduler = CosineAnnealingLR(self.optimizer, T_max=self.epochs)
+
+        self.scheduler = OneCycleLR(
+            self.optimizer,
+            max_lr=1e-2,
+            epochs=self.epochs,
+            steps_per_epoch=len(self.train_loader),
+        )
+
         self.name = name if name else ""
         self.train_losses = []
         self.val_losses = []
@@ -51,7 +64,7 @@ class Trainer:
         try:
             for epoch in range(self.epochs):
                 self.model.train()
-                train_loss = 0.0
+                epoch_loss = 0.0
                 train_loader = tqdm(
                     self.train_loader,
                     desc=f"Epoch {epoch+1}/{self.epochs} - Training",
@@ -72,15 +85,17 @@ class Trainer:
 
                     self.optimizer.zero_grad()
                     losses.backward()
+
                     self.optimizer.step()
-                    train_loss += losses.item()
-                    current_loss = train_loss / (train_loader.n + 1)
-                    lr = self.optimizer.param_groups[0]["lr"]
+                    self.scheduler.step()
+
+                    current_loss = losses.item() / self.train_loader.batch_size
+                    epoch_loss += current_loss
+                    lr = self.scheduler.get_last_lr()[0]
+                    self.train_losses.append(current_loss)
                     train_loader.set_postfix(
-                        {"Loss": f"{current_loss:.4f}", "LR": f"{lr:.6f}"}
+                        {"Loss": f"{epoch_loss:.4f}", "LR": f"{lr:.4f}"}
                     )
-                train_loss /= self.train_loader.batch_size
-                self.train_losses.append(train_loss)
                 if self.val_loader:
                     val_loss = self.validate()
                     if val_loss < best_loss:
@@ -92,10 +107,8 @@ class Trainer:
                     self.save(
                         os.path.join(self.save_dir, f"{self.name}_epoch_{epoch}.pth")
                     )
-                self.scheduler.step()
-                print(
-                    f'Epoch {epoch+1}/{self.epochs}, Train Loss: {train_loss:.4f}, Val Loss: {val_loss if self.val_loader else "N/A"}'
-                )
+
+            self.save_loss_plot()
         except KeyboardInterrupt:
             print("Training interrupted. Saving the model...")
             self.save(os.path.join(self.save_dir, "interrupted_model.pth"))
@@ -105,13 +118,15 @@ class Trainer:
     @torch.no_grad()
     def validate(self):
         self.model.eval()
+
         val_loss = 0.0
         birad_preds = []
         birad_targets = []
         density_preds = []
         density_targets = []
+
         with torch.no_grad():
-            val_loader = tqdm(self.val_loader, desc="Validation", leave=False)
+            val_loader = tqdm(self.val_loader, desc="Validation")
             for images, targets in val_loader:
                 images = [img.to(self.device) for img in images]
                 targets = [
@@ -138,39 +153,47 @@ class Trainer:
                     density_preds, density_targets, task="density"
                 )
 
-                loss = self.eval_loss(birads_probs, density_probs, targets)
+                loss = self.eval_loss(detections, birads_probs, density_probs, targets)
 
                 birad_f1 = birad_results["f1"]
                 density_f1 = density_results["f1"]
 
-                val_loss += loss.item() / self.val_loader.batch_size
+                current_loss = loss.item() / self.val_loader.batch_size
+                val_loss += current_loss
+                self.val_losses.append(val_loss)
 
                 val_loader.set_postfix(
                     {
-                        "Loss": f"{loss:.4f}",
+                        "Loss": f"{val_loss:.4f}",
                         "BIRADS_F1": f"{birad_f1:.4f}",
                         "Density_F1": f"{density_f1:.4f}",
                     }
                 )
 
-        self.val_losses.append(val_loss)
-
         self.model.train()
 
         return val_loss
 
-    def eval_loss(self, birads_logits, density_logits, targets) -> torch.Tensor:
+    def eval_loss(
+        self, detections, birads_logits, density_logits, targets
+    ) -> torch.Tensor:
         """Compute detection and classification losses during evaluation"""
         loss_dict = {}
 
         birads_targets = torch.stack([t["birads"] for t in targets]).flatten().long()
         density_targets = torch.stack([t["density"] for t in targets]).flatten().long()
 
+        detection_loss = self.box_loss(detections["boxes"], targets["boxes"])
+        classification_loss = self.detection_cls_loss(
+            detections["labels"], targets["labels"]
+        )
         birads_loss = nn.CrossEntropyLoss()(birads_logits, birads_targets)
         density_loss = nn.CrossEntropyLoss()(density_logits, density_targets)
 
-        loss_dict["birads_loss"] = birads_loss
-        loss_dict["density_loss"] = density_loss
+        loss_dict["detection_loss"] = detection_loss
+        loss_dict["cls_loss"] = classification_loss
+        loss_dict["birads_loss"] = birads_loss * 0.75
+        loss_dict["density_loss"] = density_loss * 0.50
 
         return sum(loss for loss in loss_dict.values())
 
